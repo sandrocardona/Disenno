410de22b3a0cf8a9528269562fb9373e300a4a47
sandrocardona <sandrocardona41@gmail.com>
2024-01-29 19:23:43 +0100

    29 enero casa

--- AUDIOYVIDEO/audio/main.js
+++ AUDIOYVIDEO/audio/main.js
@@ -9,7 +9,7 @@ var restart = document.getElementById("restart");
 /* REPRODUCIR O PAUSAR CANCIÓN */
 
 play.addEventListener("click",()=>{
-    var playIcon = document.getElementById("Bold");
+    let playIcon = document.getElementById("Bold");
     if(!audio.paused && !audio.ended){
         audio.pause();
         playIcon.innerHTML='<path d="M20.463,7.713l-9.1-6.677A5.317,5.317,0,0,0,2.9,5.323V18.677a5.311,5.311,0,0,0,8.46,4.287l9.105-6.677a5.315,5.315,0,0,0,0-8.574Zm-1.774,6.155-9.1,6.677A2.317,2.317,0,0,1,5.9,18.677V5.323a2.276,2.276,0,0,1,1.27-2.066A2.328,2.328,0,0,1,8.223,3a2.3,2.3,0,0,1,1.362.455l9.1,6.677a2.316,2.316,0,0,1,0,3.736Z"/>';
@@ -43,8 +43,6 @@ vol.oninput = function(){
 restart.addEventListener("click",()=>{
     audio.currentTime = 0;
 });
-
-
 
 /* MOSTRAR TIEMPO DE AUDIO */
 
