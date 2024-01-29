1f8a40969390e961e24367dbd574725f3ecbdcd7
sandrocardona <sandrocardona41@gmail.com>
2024-01-29 13:40:49 +0100

    29 enero clase

--- AUDIOYVIDEO/audio/main.js
+++ AUDIOYVIDEO/audio/main.js
@@ -1,9 +1,39 @@
+const audio = document.getElementById("audio1");
+
 function display(){
-    if(!audio1.paused && !audio1.ended){
-        audio1.pause();
+    let audio = document.getElementById("audio1");
+    if(!audio.paused && !audio.ended){
+        audio.pause();
         (console.log("audio pausado"));
     } else {
-        audio1.play()
+        audio.play()
         .then(console.log("audio reproduciendose"));
     }
 }
+
+/* CAMBIAR VOLUMEN */
+console.log(audio.volume);
+let rango = document.getElementById("rango");
+rango.addEventListener('change', () =>{
+    audio.volume = rango.ariaValueMax;
+    console.log(audio.volume);
+})
+
+/* MUTEAR EL AUDIO */
+
+function mute(){
+    let audio = document.getElementById("audio1");
+    if(audio.volume>0){
+        audio.volume=0;
+    } else {
+        audio.volume=0.5;
+    }
+    console.log(audio.volume);
+}
+
+/* Mostrar tiempo de audio */
+
+audio.addEventListener("timeupdate", () => {
+    document.getElementById("timer").innerHTML=Number.parseInt(audio.currentTime);
+    console.log("Current second: ", Math.round(audio.currentTime));
+  });
