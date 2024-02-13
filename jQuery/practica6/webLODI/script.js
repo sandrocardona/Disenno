cd075421679fcc6091ad5dbb82d5dd7a7c9615b1
sandrocardona <sandrocardona41@gmail.com>
2024-02-13 14:15:32 +0100

    13 febrero clase

---
+++ jQuery/practica6/webLODI/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        $('header').css("position","fixed");
+        $('header').css("width","100%");
+        $('header').css("background","white");
+        $('header').css("opacity","0.8");
+    })
+})
