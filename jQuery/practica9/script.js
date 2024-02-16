2d361d3dae9fcdf2f5dcc946ac7dd1a212b3889c
sandrocardona <sandrocardona41@gmail.com>
2024-02-16 14:40:36 +0100

    16 febrero clase

--- jQuery/practica9/script.js
+++ jQuery/practica9/script.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
-
+    var colored = false;
+    $("header > nav#menu-principal > label").click(function(){
+        if(colored){
+            $(this).animate({"background-color":"black"})
+        }else{
+            $(this).animate({"background-color":"white"})
+        }
+        colored = !colored
+    })
 })
