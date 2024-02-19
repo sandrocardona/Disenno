89a65c204cbd2501fafe37d36516049d36eace7e
sandrocardona <sandrocardona41@gmail.com>
2024-02-19 13:55:50 +0100

    19 febrero clase

--- jQuery/webJUSTFAB/script.js
+++ jQuery/webJUSTFAB/script.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
+    /* ocultar menu */
+    $("ul#menu").css("display", "none");
+
+    /* Boton volverarriba con scroll */
     $(this).on(
         "scroll",function(){
             if($(this).scrollTop() > 10){
@@ -6,6 +10,20 @@ $(document).ready(function(){
             }else{
                 $('div#volverarriba').fadeOut();
             }
+        }
+    )
+
+    /* mostrar menú al hacer click */
+    $("header > div > nav#menu-principal > span").on(
+        "click",function(){
+            $("nav#menu-principal").children("ul").slideToggle();
+        }
+    )
+
+    /* hacer click en pestañas li del menu desplegado */
+    $("nav#menu-principal > ul#menu").on(
+        "click",function(){
+            $(this).children().slideToggle();
         }
     )
 });
