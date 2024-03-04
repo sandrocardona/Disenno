ce89fc00fc3d919e35a1687c636d1a4a1f184dcc
sandrocardona <sandrocardona41@gmail.com>
2024-03-04 13:49:19 +0100

    4 marzo

---
+++ jQuery/Webusefab/jq/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $("nav#menu-principal").on({
+        click:function(){
+            $("div#oscuro").show(),
+
+            $("ul#menu").animate({
+                left: "0"
+            }, 1500);
+
+            $("div#desplazable").animate({
+                left:"18rem"
+            }, 1500);
+        },
+
+    })
+})
