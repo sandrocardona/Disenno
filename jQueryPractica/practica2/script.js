195c12112e8b1d95ab8dd5d7714e10f58be065f4
sandrocardona <sandrocardona41@gmail.com>
2024-03-05 14:33:51 +0100

    5 marzo

--- jQueryPractica/practica2/script.js
+++ jQueryPractica/practica2/script.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
-    $(".noticia").click(function(){
-        $(this).children("h3").show();
-    });
+    $("article.noticia").on({
+        click:function(){
+            $(this).children("h3").show();
+        },
 
-    $(".noticia").dblclick(function(){
-        $(this).children("h3").hide();
-    });
+        dblclick:function(){
+            $(this).children("h3").hide();
+        }
+    })
 })
